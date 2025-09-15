1cf7de3bdbc229faf521de4ca0d24c364280761d
cyfolas <163515367+cyfolas@users.noreply.github.com>
2025-09-15 09:01:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+enum MilitaryStatus
+{
+    liable,
+    nonliable
+}
+
+struct Person
+{
+    public string surname;
+    public int age;
+    public MilitaryStatus ms;
+}
+
+class Program
+{
+    static void Main()
+    {
+        const int SIZE = 3;
+        Person[] people = new Person[SIZE];
+
+        Console.WriteLine("Заполнение массива структур:");
+        Console.WriteLine("============================");
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            Console.WriteLine("\nЧеловек #" + (i + 1) + ":");
+
+            Console.Write("Введите фамилию: ");
+            people[i].surname = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            people[i].age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Военнообязанный? (1 - да, 0 - нет): ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            people[i].ms = (temp == 1) ? MilitaryStatus.liable : MilitaryStatus.nonliable;
+        }
+
+        int youngest_age_1 = 1000;
+        string youngest_surname_1 = "";
+        foreach (Person person in people)
+        {
+            if (person.ms == MilitaryStatus.liable && person.age < youngest_age_1)
+            {
+                youngest_age_1 = person.age;
+                youngest_surname_1 = person.surname;
+            }
+        }
+        Console.WriteLine("Фамилия младшего по возрасту среди военнообязанных: " + youngest_surname_1);
+
+        int oldest_age_0 = 0;
+        int oldest_age_1 = 0;
+        string oldest_surname_0 = "";
+        string oldest_surname_1 = "";
+
+        foreach (Person person in people)
+        {
+            if (person.ms == MilitaryStatus.liable && person.age > oldest_age_1)
+            {
+                oldest_age_1 = person.age;
+                oldest_surname_1 = person.surname;
+            }
+            if (person.ms == MilitaryStatus.nonliable && person.age > oldest_age_0)
+            {
+                oldest_age_0 = person.age;
+                oldest_surname_0 = person.surname;
+            }
+        }
+        Console.WriteLine("Фамилия старшего по возрасту среди военнообязанных: " + oldest_surname_1);
+        Console.WriteLine("Фамилия старшего по возрасту среди не военнообязанных: " + oldest_surname_0);
+    }
+}
---
+++ lab1_2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+enum MilitaryStatus {
+    nonliable,
+    liable,
+};
+
+struct Person {
+
+	string surname;
+	int age;
+	MilitaryStatus ms;
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int SIZE = 3;
+    Person people[SIZE];
+
+    cout << "Заполнение массива структур:" << endl;
+    cout << "============================" << endl;
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "\nЧеловек #" << i + 1 << ":" << endl;
+
+        
+        cout << "Введите фамилию: ";
+        cin >> people[i].surname;
+
+        
+        cout << "Введите возраст: ";
+        cin >> people[i].age;
+
+    
+        cout << "Военнообязанный? (1 - да, 0 - нет): ";
+        int temp;
+        cin >> temp;
+        people[i].ms = MilitaryStatus(temp);
+    }
+    int youngest_age_1 = 1000;
+    string youngest_surname_1;
+    for (Person person : people)
+    {
+        if (person.ms == liable && person.age < youngest_age_1)
+        {
+            youngest_age_1 = person.age;
+            youngest_surname_1 = person.surname;
+
+        }
+    }
+    cout << "Фамилия младшего по возрасту среди военнообязанных: " << youngest_surname_1 << endl;
+    int oldest_age_0 = 0, oldest_age_1 = 0;
+    string oldest_surname_0, oldest_surname_1;
+
+    for (Person person : people)
+    {
+        if (person.ms == liable && person.age > oldest_age_1)
+        {
+            oldest_age_1 = person.age;
+            oldest_surname_1 = person.surname;
+
+        }
+        if (person.ms == nonliable && person.age > oldest_age_0)
+        {
+            oldest_age_0 = person.age;
+            oldest_surname_0 = person.surname;
+
+        }
+    }
+    cout << "Фамилия старшего по возрасту среди военнообязанных: " << oldest_surname_1 << endl;
+    cout << "Фамилия старшего по возрасту среди не военнообязанных: " << oldest_surname_0;
+}
+
